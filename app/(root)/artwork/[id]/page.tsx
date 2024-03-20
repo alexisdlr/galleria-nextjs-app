84a70c9ce91455d3e216b6700ad6183b7fbc9dcf
alexisdlr <alexisdlr3009@hotmail.com>
2024-03-19 22:12:26 -0600

    responsive artwork detail page

--- app/(root)/artwork/[id]/page.tsx
+++ app/(root)/artwork/[id]/page.tsx
@@ -35,23 +35,23 @@ const ArtworkDetail = async ({
             <LightBox images={[galleryImg]} />
 
             <div className=" absolute w-56 h-32 lg:w-72 lg:h-56 lg:flex flex-col items-start justify-center bg-white -bottom-10 -left-1 p-4 lg:bottom-auto lg:left-auto lg:-top-3 lg:-right-16 lg:p-10">
-              <h2 className="font-bold text-xl lg:text-4xl">{artwork?.name}</h2>
+              <h2 className="font-bold text-2xl lg:text-4xl">{artwork?.name}</h2>
               <h3 className="text-xs text-gray-500 my-3">
                 {artwork?.artist?.name}
               </h3>
             </div>
-            <div className="absolute left-3 -bottom-16 lg:left-auto lg:-bottom-8 lg:-right-16">
+            <div className="absolute left-3 -bottom-20 lg:left-auto lg:-bottom-8 lg:-right-16">
               <Image
                 src={artistImg}
                 alt={artwork?.name}
                 width={150}
                 height={150}
-                className="w-[64px] h-[64px] lg:w-[150px] lg:h-[150px]"
+                className="w-[70px] h-[70px] lg:w-[150px] lg:h-[150px]"
               />
             </div>
           </div>
           <div className="h-full flex flex-col items-start justify-center gap-10 w-ful lg:w-auto lg:max-w-96 lg:ml-20 mt-6 lg:mt-0 text-[#7D7D7D] relative">
-            <span className="font-bold text-[#F3F3F3] text-[90px] lg:text-[170px] absolute left-20 -top-16 lg:-top-14 lg:left-6">
+            <span className="font-bold text-[#F3F3F3] text-[90px] lg:text-[170px] absolute left-1/3 -top-16 lg:-top-14 lg:left-6">
               {artwork.year}
             </span>
             <p className="text-sm z-10 leading-6 ">{artwork.description}</p>
