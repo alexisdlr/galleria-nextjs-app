8d122b168c574e0c2c3c617d2a60bb54e777cde5
alexisdlr <alexisdlr3009@hotmail.com>
2024-03-19 20:57:38 -0500

    artwork detail

--- app/(root)/artwork/[id]/page.tsx
+++ app/(root)/artwork/[id]/page.tsx
@@ -28,7 +28,7 @@ const ArtworkDetail = async ({
     <>
       <div className="my-10">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
-          <div className="h-full relative w-full">
+          <div className="h-full relative ">
             <Image
               src={artwork?.images?.hero.large}
               alt={artwork?.name}
@@ -36,29 +36,29 @@ const ArtworkDetail = async ({
               height={450}
             />
             <LightBox images={[artwork?.images?.gallery]} />
-            <div className="absolute h-32 w-52 p-6 lg:h-56 flex flex-col items-start justify-center bg-white -bottom-10 -left-2 lg:-top-3 lg:-right-16 lg:p-10">
-              <h2 className="font-bold lg:text-4xl">{artwork?.name}</h2>
+            <div className="absolute w-72 h-56 flex flex-col items-start justify-center bg-white -top-3 -right-16 p-10">
+              <h2 className="font-bold text-4xl">{artwork?.name}</h2>
               <h3 className="text-xs text-gray-500 my-3">
                 {artwork?.artist?.name}
               </h3>
             </div>
-            <div className="absolute -bottom-20 left-3 z-10 lg:-bottom-8 lg:-right-16">
+            <div className="absolute -bottom-8 -right-16">
               <Image
                 src={artwork?.artist?.image}
                 alt={artwork?.name}
                 width={150}
                 height={150}
-                className="image"
               />
             </div>
           </div>
-          <div className="h-full flex flex-col items-start justify-center gap-10 max-w-full lg:max-w-96 lg:ml-20 text-[#7D7D7D] relative mt-20 lg:mt-0">
-            <span className="font-bold text-[#F3F3F3] text-8xl lg:text-[170px] absolute -top-14 left-6">
+          <div className="h-full flex flex-col items-start justify-center gap-10 max-w-96 ml-20 text-[#7D7D7D] relative">
+            <span className="font-bold text-[#F3F3F3] text-[170px] absolute -top-14 left-6">
               {artwork.year}
             </span>
             <p className="text-sm z-10 leading-6">{artwork.description}</p>
             <Link
               href={artwork?.source}
+              target="_blank"
               className="uppercase text-xs underline tracking-wider"
             >
               Go to source
